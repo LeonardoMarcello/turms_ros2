a07a91af6f5a8da731cb26696d39ea86948e95e8
LeonardoMarcello <69303465+LeonardoMarcello@users.noreply.github.com>
2025-12-02 23:15:09 +0100

    Update read and write methods to include time and period

--- turms_ros2control/include/turms_ros2control/turms_system.hpp
+++ turms_ros2control/include/turms_ros2control/turms_system.hpp
@@ -81,10 +81,10 @@ public:
   CallbackReturn on_deactivate(const rclcpp_lifecycle::State & State) override;
 
   TURMS_HARDWARE_PUBLIC
-  hardware_interface::return_type read() override;
+  hardware_interface::return_type read(const rclcpp::Time & time,const rclcpp::Duration & period) override;
 
   TURMS_HARDWARE_PUBLIC
-  hardware_interface::return_type write() override;
+  hardware_interface::return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
 private:
 
